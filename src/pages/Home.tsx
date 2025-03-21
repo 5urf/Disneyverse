85bcd8fa690d78e47793592de897b4a9ee2e844f
LeeYunHak <sza0203@naver.com>
2025-03-22 02:28:20 +0900

    fix(Home): style 및 data filter 임시 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,28 @@
-import useDisneyCharacters from "../hooks/useDisneyCharacters";
+import styled from "styled-components";
+import CharacterGrid from "../components/CharacterGrid";
+import useGetDisneyCharacters from "../hooks/useGetDisneyCharacters";
+
+const HomeContainer = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 const Home = () => {
-  const { data, isLoading } = useDisneyCharacters();
-  console.log("📢[Home.tsx:5]: isLoading: ", isLoading);
-  console.log("📢[Home.tsx:5]: data: ", data);
-  return <div>Home</div>;
+  const { data: disneyCharacters, isLoading } = useGetDisneyCharacters();
+  //NOTE - 임시
+  const filteredDisneyCharacters = disneyCharacters
+    ?.filter((characters) => characters.imageUrl)
+    .slice(0, 50);
+
+  //NOTE - 스켈레톤 UI 적용할 것
+  if (isLoading || !filteredDisneyCharacters) return <p>Loading...</p>;
+
+  return (
+    <HomeContainer>
+      <CharacterGrid characters={filteredDisneyCharacters} />
+    </HomeContainer>
+  );
 };
 
 export default Home;
